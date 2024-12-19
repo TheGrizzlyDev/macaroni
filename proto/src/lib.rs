51c4e355d6de47d40a5c36098dcb80e5a470660e
TheGrizzlyDev <tonio9681@gmail.com>
2024-12-20 00:26:20 +0100

    Restructure project and make it daemonless

--- proto/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("management_descriptor");
-pub mod management {
-    tonic::include_proto!("management");
-}
-pub mod sandbox {
-    tonic::include_proto!("sandbox");
-}
