c011e844c1b446533368c8902a36135161c7846a
TheGrizzlyDev <tonio9681@gmail.com>
2024-12-30 01:11:48 +0100

    specify callcon in getcwd

--- zig_rewrite/src/cwd.zig
+++ zig_rewrite/src/cwd.zig
@@ -6,7 +6,7 @@ pub fn cwd(pathResolver: *PathResolver) type {
     return struct {
         pub var cwdPathResolver: *PathResolver = pathResolver;
 
-        pub fn getcwd(buf: [*c]u8, size: usize) [*c]u8 {
+        pub fn getcwd(buf: [*c]u8, size: usize) callconv(.C) [*c]u8 {
             cwdPathResolver.bla();
             std.debug.print("called getcwd\n", .{});
             return libsystem.getcwd(buf, size);
