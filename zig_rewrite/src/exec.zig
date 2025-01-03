e62e7c3de5befde789ad362954f837d29fc49c7f
TheGrizzlyDev <tonio9681@gmail.com>
2025-01-04 00:34:56 +0100

    Actually add exec stuff

---
+++ zig_rewrite/src/exec.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const libsystem = @import("./libsystem.zig");
+const PathResolver = @import("./PathResolver.zig");
+
+// TODO implement popen and system once you have a strategy for SIP binaries since they both use /bin/sh
+pub fn exec(pathResolver: *PathResolver, allocator: *std.mem.Allocator) type {
+    return struct {
+        var execPathResolver = pathResolver;
+        var execAllocator = allocator;
+
+        pub fn execve(path: [*c]const u8, argv: [*c][*c]const u8, envp: [*c][*c]const u8) callconv(.C) c_int {
+            const resolved_path = execPathResolver.resolve(execAllocator.*, std.mem.span(path), .{ .sentinel = 0 }) catch {
+                libsystem.setErrno(std.posix.E.NOENT);
+                return -1;
+            };
+            // TODO: ensure that the env variables required by macaroni are forwarded
+            return libsystem.execve(@ptrCast(resolved_path), argv, envp);
+        }
+    };
+}
