8641778b8ecb0edbc720158d26e5dcac2214d49d
TheGrizzlyDev <tonio9681@gmail.com>
2025-01-10 00:07:58 +0100

    build macaroni as a universal library

--- build.zig
+++ build.zig
@@ -1,21 +1,40 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addSharedLibrary(.{
+    const target_arm64 = b.resolveTargetQuery(.{
+        .cpu_arch = .aarch64,
+    });
+
+    const lib_arm64 = b.addSharedLibrary(.{
         .name = "macaroni",
         .root_source_file = b.path("src/root.zig"),
-        .target = target,
+        .target = target_arm64,
         .optimize = optimize,
     });
 
-    b.installArtifact(lib);
+    const target_amd64 = b.resolveTargetQuery(.{
+        .cpu_arch = .x86_64,
+    });
+
+    const lib_amd64 = b.addSharedLibrary(.{
+        .name = "macaroni",
+        .root_source_file = b.path("src/root.zig"),
+        .target = target_amd64,
+        .optimize = optimize,
+    });
+
+    const run_lipo = b.addSystemCommand(&.{"lipo"});
+    run_lipo.addArgs(&.{ "-create", "-output" });
+    const lib = run_lipo.addOutputFileArg("libsystem.dylib");
+    run_lipo.addArtifactArg(lib_amd64);
+    run_lipo.addArtifactArg(lib_arm64);
+
+    b.getInstallStep().dependOn(&b.addInstallBinFile(lib, "libsystem.dylib").step);
 
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
-        .target = target,
         .optimize = optimize,
     });
 
